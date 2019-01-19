896887029d0825f74d01876c9709665dbfcdcddb
Disaac1 <disaac1256@gmail.com>
2019-01-18 23:58:17 -0600

    Fixed Compile issue...
    Didn't notice we changed how we did things...

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -13,21 +13,21 @@ public class Controls {
     private static SendableChooser<XboxController> operatorControllerChooser = new SendableChooser<>();
     private static final XboxController controllerOne = new XboxController(0);
     private static final XboxController controllerTwo = new XboxController(2);
-    private static final ControlSet controlSet = new ControlSet(driverControllerChooser, operatorControllerChooser);
+    //private static final ControlSet controlSet = new ControlSet(driverControllerChooser, operatorControllerChooser);
 
     public static void robotInit(){
-        driverChooser.setDefaultOption("Jorge Driver", new JorgeDriver(controlSet));
-        driverChooser.addOption("Brandon Driver", new BrandonDriver(controlSet));
-        driverChooser.addOption("Hunter Driver", new HunterDriver(controlSet));
+        driverChooser.setDefaultOption("Jorge Driver", new JorgeDriver(driverControllerChooser));
+        driverChooser.addOption("Brandon Driver", new BrandonDriver(driverControllerChooser));
+        driverChooser.addOption("Hunter Driver", new HunterDriver(driverControllerChooser));
         SmartDashboard.putData("Driver", driverChooser);
 
         driverControllerChooser.setDefaultOption("Driver Controller: 1", controllerOne);
         driverControllerChooser.addOption("Driver Controller: 2", controllerTwo);
         SmartDashboard.putData("Driver Controller", driverControllerChooser);
 
-        operatorChooser.setDefaultOption("Jorge Operator", new JorgeOperator(controlSet));
-        operatorChooser.addOption("Brandon Operator", new BrandonOperator(controlSet));
-        operatorChooser.addOption("Hunter Operator", new HunterOperator(controlSet));
+        operatorChooser.setDefaultOption("Jorge Operator", new JorgeOperator(driverControllerChooser));
+        operatorChooser.addOption("Brandon Operator", new BrandonOperator(driverControllerChooser));
+        operatorChooser.addOption("Hunter Operator", new HunterOperator(driverControllerChooser));
         SmartDashboard.putData("Operator", operatorChooser);
 
         operatorControllerChooser.setDefaultOption("Operator Controller: 1", controllerOne);
