03d6ee33fd7c0e0b80b553513e54ff3de66a8a3a
Andrew Gasser <agasser01@hotmail.com>
2019-03-29 17:49:23 -0500

    Move LOCK_HATCH and SHUTTLE_CARGO up after match 62

--- src/main/java/frc/robot/subsystems/PivotPosition.java
+++ src/main/java/frc/robot/subsystems/PivotPosition.java
@@ -2,8 +2,8 @@ package frc.robot.subsystems;
 
 public enum PivotPosition {
     UNLOCK_HATCH(0),
-    LOCK_HATCH(-375),
-    SHUTTLE_CARGO(-850),
+    LOCK_HATCH(-300),
+    SHUTTLE_CARGO(-900),
     ROCKET_CARGO(-625),
     CLIMB(-1300),
     REST(0);
